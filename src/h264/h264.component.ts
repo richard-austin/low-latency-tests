3448c6d2c4c01cd34d7cdeaf402b191078b5c04b
richard-austin <richard.david.austin@gm>
2025-03-31 21:00:05 +0100

    * Web worker set up to handle incoming websocket data, keyframe identification and video decoding. * Finally achieved WebRTC equivalent performance! The problem was when the browser id using the GPU for decoding, it was much slower than using hardware acceleration. (I'll have to look into that).

--- src/h264/h264.component.ts
+++ src/h264/h264.component.ts
@@ -16,86 +16,29 @@ export class H264Component implements OnInit, AfterViewInit{
 
     file!: HTMLInputElement;
     video!: HTMLVideoElement;
-    ws!: WebSocket;
-
     // @ts-ignore (MediaStreamTrackGenerator not in lib.dom.d.ts for some reason)
     readonly trackGenerator = new MediaStreamTrackGenerator({ kind: 'video' });
     readonly defaultWriter = this.trackGenerator.writable.getWriter();
 
-    readonly decoder = new VideoDecoder({
-        output: async (frame) => {
-            await this.defaultWriter.write(frame);
-            frame.close();
-            await this.defaultWriter.ready;
-           // this.defaultWriter.releaseLock();
-        },
-        error: (e) => console.warn(e.message),
-    });
-
-    process = async (data: Uint8Array): Promise<void> => {
-
-        if (this.decoder.state !== "configured") {
-            const config = {codec: "avc1.4d002a", optimizeForLatency: true};
-            this.decoder.configure(config);
-        }
-        const chunk = new EncodedVideoChunk({
-            timestamp: (performance.now() + performance.timeOrigin) * 1000,
-            type: (data[4] & 0x0f) === 7 ? "key" : "delta",
-            data,
-        });
-        this.decoder.decode(chunk);
-    }
 
     async start(): Promise<void> {
-        const process = this.process;
+    //    const process = this.process;
         this.video.srcObject = new MediaStream([this.trackGenerator])
         this.video.onloadedmetadata = () => {
             this.video.play().then();
         }
 
-        this.ws = new WebSocket("/ws/stream?suuid=stream1");
-        this.ws.binaryType = 'arraybuffer';
-        this.ws.onmessage = (event: MessageEvent) => {
-            processChunk(event.data);
-        };
-
-        let buffer = new Uint8Array();
-
-        let keyFrameReceived: boolean = false;
-        let keyFrameBuffers: Uint8Array[] = [];
-
-        // @ts-ignore
-        async function processChunk(value: ArrayBuffer): Promise<void> {
-            buffer = new Uint8Array(value);
-            const i = 0
-            const isStartFrame = (buffer[i] === 0 && buffer[i + 1] === 0 && buffer[i + 2] === 0 && buffer[i + 3] === 1);
-            const isKeyFrame = isStartFrame && (((buffer[i + 4] & 0x07) === 7));
-            if (isKeyFrame) {
-                keyFrameReceived = true;
-                keyFrameBuffers.push(buffer);
-            }
-            else if(!isStartFrame && !isKeyFrame && keyFrameReceived)
-                    keyFrameBuffers.push(buffer);
-            else if (!isKeyFrame && isStartFrame && keyFrameReceived) {
-                if (keyFrameBuffers.length > 0) {
-                    let totalLength = 0;
-                    keyFrameBuffers.forEach((element: Uint8Array) => {
-                        totalLength += element.length;
-                    });
-                    let offset = 0;
-                    const mergedArray = new Uint8Array(totalLength);
-                    keyFrameBuffers.forEach((element: Uint8Array) => {
-                        mergedArray.set(element, offset);
-                        offset += element.length;
-                    });
-                    keyFrameBuffers = [];
-                    await process(mergedArray);
-                }
-                await process(buffer);
-                }
-
-            // buffer = buffer.slice(start);
-            //return reader.read().then(processChunk);
+        if (typeof Worker !== 'undefined') {
+            // Create a new media feeder web worker
+            const worker = new Worker(new URL('./media-feeder.worker', import.meta.url));
+            worker.onmessage = async ({data, type}) => {
+                await this.defaultWriter.write(data);
+                await this.defaultWriter.ready;
+            };
+            worker.postMessage({url: "/ws/stream?suuid=stream1"})
+        } else {
+            // Web workers are not supported in this environment.
+            // You should add a fallback so that your program still executes correctly.
         }
     }
 
---
+++ src/h264/media-feeder.worker.ts
@@ -0,0 +1,86 @@
+/// <reference lib="webworker" />
+
+addEventListener('message', ({ data }) => {
+  setUpWebsocketConnection(data.url);
+ });
+
+
+let decoder: VideoDecoder;
+let keyFrameReceived: boolean = false;
+
+function setupDecoder() : void {
+  keyFrameReceived = false;
+  decoder = new VideoDecoder({
+    output: async (frame) => {
+      postMessage(frame);
+      frame.close();
+    },
+    error: (e) => {
+      console.warn(e.message);
+      setupDecoder();
+    },
+  });
+}
+
+setupDecoder();
+
+let processMessage = async (data: Uint8Array): Promise<void> => {
+ // let processStart = performance.now();
+  if (decoder.state !== "configured") {
+    const config = {codec: "avc1.4d002a", optimizeForLatency: true};
+    decoder.configure(config);
+  }
+  const chunk = new EncodedVideoChunk({
+    timestamp: (performance.now()) * 1000,
+    type: (data[4] & 0x0f) === 7 ? "key" : "delta",
+    data: data,
+  });
+  decoder.decode(chunk);
+  // console.log("process time: "+(performance.now()-processStart))
+}
+
+function setUpWebsocketConnection(url: string) {
+  let ws = new WebSocket(url);
+  ws.binaryType = 'arraybuffer';
+  ws.onmessage = (event: MessageEvent) => {
+    processChunk(event.data).then();
+  };
+
+  let buffer = new Uint8Array();
+
+  let keyFrameBuffers: Uint8Array[] = [];
+
+  async function processChunk(value: ArrayBuffer): Promise<void> {
+  //  let processChunkStart = performance.now();
+    buffer = new Uint8Array(value);
+    const i = 0
+    const isStartFrame = (buffer[i] === 0 && buffer[i + 1] === 0 && buffer[i + 2] === 0 && buffer[i + 3] === 1);
+    const isKeyFrame = isStartFrame && (((buffer[i + 4] & 0x07) === 7));
+    if (isKeyFrame) {
+      keyFrameReceived = true;
+      keyFrameBuffers.push(buffer);
+    }
+    else if(!isStartFrame && !isKeyFrame && keyFrameReceived)
+      keyFrameBuffers.push(buffer);
+    else if (!isKeyFrame && isStartFrame && keyFrameReceived) {
+      if (keyFrameBuffers.length > 0) {
+        let totalLength = 0;
+        keyFrameBuffers.forEach((element: Uint8Array) => {
+          totalLength += element.length;
+        });
+        let offset = 0;
+        const mergedArray = new Uint8Array(totalLength);
+        keyFrameBuffers.forEach((element: Uint8Array) => {
+          mergedArray.set(element, offset);
+          offset += element.length;
+        });
+        keyFrameBuffers = [];
+        await processMessage(mergedArray);
+      }
+      await processMessage(buffer);
+    }
+    // console.log("processChunk time: "+(performance.now()-processChunkStart))
+    // buffer = buffer.slice(start);
+    //return reader.read().then(processChunk);
+  }
+}
